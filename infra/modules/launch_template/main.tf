afeb023120a17d0a9674559ebe48c1cc71ace10a
Lucas Aragão <lucas.arags@gmail.com>
2025-01-17 13:32:04 -0300

    refactoring launch_template module

--- infra/modules/launch_template/main.tf
+++ infra/modules/launch_template/main.tf
@@ -1,3 +1,7 @@
+locals {
+  app_index_hash = filemd5("${path.module}/../app/index.html")
+}
+
 resource "aws_launch_template" "launch_template" {
   name          = var.launch_template_name
   image_id      = var.ami
@@ -7,7 +11,7 @@ resource "aws_launch_template" "launch_template" {
     device_index    = 0
     security_groups = [var.security_group_id]
   }
-  
+
   tag_specifications {
     resource_type = "instance"
 
@@ -16,5 +20,12 @@ resource "aws_launch_template" "launch_template" {
     }
   }
 
-  user_data = base64encode(file("${path.module}/../common/install-docker.sh"))
+  user_data = base64encode(
+    file("${path.module}/../common/install-docker.sh") 
+    + "\n# Index hash: ${local.app_index_hash}"
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
